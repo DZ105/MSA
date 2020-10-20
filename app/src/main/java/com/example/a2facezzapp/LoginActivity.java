a765f6543235fbdfc683e6b3341217f2f3b83fd1
daividrus <drus425@gmail.com>
2020-10-20 22:25:05 +0300

    login action demo added

--- app/src/main/java/com/example/a2facezzapp/LoginActivity.java
+++ app/src/main/java/com/example/a2facezzapp/LoginActivity.java
@@ -4,16 +4,15 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
-
 import android.content.Intent;
-import android.drm.DrmStore;
 import android.os.Bundle;
 import android.util.Log;
 import android.util.Patterns;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -24,43 +23,39 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class RegisterActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
+
     //views
     EditText mEmailInput, mPasswordInput;
-    Button mRegisterBtn;
-    TextView mHaveAccount;
+    Button mLoginBtn;
+    TextView noAccountTV;
 
-    //displaying a progress bar while registering
-    ProgressBar progressBar;
-
-//    declare an instance of FirebaseAuth
+    //firebase
     private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        setContentView(R.layout.activity_login);
 
         //Actionbar and its title
         ActionBar actionBar = getSupportActionBar();
-        actionBar.setTitle("Create account");
+        actionBar.setTitle("Login");
 
         //enable back button
         actionBar.setDisplayHomeAsUpEnabled(true);
         actionBar.setDisplayShowHomeEnabled(true);
 
-        //initialize the activity
+        //init
         mEmailInput = findViewById(R.id.emailInput);
         mPasswordInput = findViewById(R.id.passwordInput);
-        mRegisterBtn = findViewById(R.id.registerBtn);
-        progressBar = new ProgressBar(this);
-        mHaveAccount = findViewById(R.id.have_accountTV);
+        mLoginBtn = findViewById(R.id.loginBtn);
+        noAccountTV = findViewById(R.id.no_accountTV);
 
-        //initialize firebase auth
         mAuth = FirebaseAuth.getInstance();
 
-        //handle register button
-        mRegisterBtn.setOnClickListener(new View.OnClickListener() {
+        //click on login button
+        mLoginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //input email and password
@@ -72,44 +67,37 @@ public class RegisterActivity extends AppCompatActivity {
                     //set error and focus email input box
                     mEmailInput.setError("Invalid email");
                     mEmailInput.setFocusable(true);
-                }
-                else if (password.length() < 6){
-                    //set error and focus password input box
-                    mPasswordInput.setError("Password length should be at least 6 characters");
-                    mPasswordInput.setFocusable(true);
-                }
-                else {
-                    //register the user
-                    registerUser(email, password);
+                }else {
+                    //if email is valid, login
+                    loginUser(email, password);
                 }
             }
         });
-        //handle login textView click listener
-        mHaveAccount.setOnClickListener(new View.OnClickListener() {
+
+        //if user click no account
+        noAccountTV.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+                finish();
             }
         });
     }
 
-    private void registerUser(String email, String password) {
-        //email and password are valid, show progress bar and start registering the user
-
-        mAuth.createUserWithEmailAndPassword(email, password)
+    private void loginUser(String email, String password) {
+        mAuth.signInWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
-                            // Sign in success, start register activity.
+                            // Sign in success, update UI with the signed-in user's information
                             FirebaseUser user = mAuth.getCurrentUser();
-                            Toast.makeText(RegisterActivity.this, "Registered...\n" + user.getEmail(), Toast.LENGTH_SHORT).show();
-                            startActivity(new Intent(RegisterActivity.this, ProfileActivity.class));
+                            //if user is logged in, start profile activity
+                            startActivity(new Intent(LoginActivity.this, ProfileActivity.class));
                             finish();
                         } else {
                             // If sign in fails, display a message to the user.
-
-                            Toast.makeText(RegisterActivity.this, "Authentication failed.",
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
                                     Toast.LENGTH_SHORT).show();
 
                         }
@@ -117,14 +105,18 @@ public class RegisterActivity extends AppCompatActivity {
                 }).addOnFailureListener(new OnFailureListener() {
             @Override
             public void onFailure(@NonNull Exception e) {
-                //error and show the error message
-                Toast.makeText(RegisterActivity.this, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+                //show error
+                Toast.makeText(LoginActivity.this, "" + e.getMessage(),
+                        Toast.LENGTH_SHORT).show();
             }
         });
     }
+
     @Override
     public boolean onSupportNavigateUp() {
         onBackPressed();
         return super.onSupportNavigateUp();
     }
+
+
 }
--- app/src/main/java/com/example/a2facezzapp/ProfileActivity.java
+++ app/src/main/java/com/example/a2facezzapp/ProfileActivity.java
@@ -1,11 +1,25 @@
 package com.example.a2facezzapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    //views
+    TextView mProfileTV;
+
+    //firebase authentication
+    FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +29,51 @@ public class ProfileActivity extends AppCompatActivity {
         //Actionbar and its title
         ActionBar actionBar = getSupportActionBar();
         actionBar.setTitle("Profile");
+
+        //initialize
+        firebaseAuth = FirebaseAuth.getInstance();
+        mProfileTV = findViewById(R.id.profileTV);
+    }
+
+    private void checkUserStatus() {
+        //get current user
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+        if(user != null) {
+            //if it is sign in stay here
+            //set email of logged in user
+            mProfileTV.setText(user.getEmail());
+        }else {
+            //if it is not sign in, go to main activity
+            startActivity(new Intent(ProfileActivity.this, MainActivity.class));
+            finish();
+        }
+
+    }
+
+    @Override
+    protected void onStart() {
+        //check on start of app
+        checkUserStatus();
+        super.onStart();
+    }
+
+    //options menu
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    //handle menu
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.action_logout) {
+            firebaseAuth.signOut();
+            checkUserStatus();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
